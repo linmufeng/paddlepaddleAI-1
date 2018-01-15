d4853c3ab4f45856a6b1b1f9f761cda06409ff3b
Lin Mufeng <linmufeng@yeah.net>
2018-01-15 15:20:16 +0800

    add pro_processing
    input : vedio_id(eg : "141738500")
    return : a 1-dim label_array

---
+++ train/data_prepare/pro_processing.py
@@ -0,0 +1,48 @@
+import cPickle
+import sys
+import json
+import pandas as pd
+import numpy as np
+import math
+
+def load_json(file):
+    with open(file) as json_file:
+        data = json.load(json_file)
+        return data
+
+def pro_processing(id):
+    json_data= load_json("/home/kesci/work/Broad/INFO/meta.json")
+    database=json_data['database']
+    len_image=0
+    for video_name in database.keys():
+        if (video_name == id):
+            video_info=database[video_name]
+            video_subset=video_info["subset"]
+            try:
+                with open("/mnt/BROAD-datasets/video/"+video_subset+"/"+str(video_name)+".pkl",'rb') as f:
+                    img_fea=cPickle.load(f)
+                    len_image=len(img_fea)
+            except:
+                print "Not found!"
+                return
+            
+            # array init
+            labelArr = np.zeros([len_image])
+            segment_info=video_info['annotations']
+            
+            # add seg flag
+            for seg_info in segment_info:
+                begin = float(seg_info["segment"][0])
+                begin = math.ceil(begin)
+                begin = int(begin)
+                
+                end = float(seg_info["segment"][1])
+                end = math.floor(end)
+                end = int(end)
+                labelArr[begin:end+1] = 1
+    return labelArr
+
+def getLabelArray(id):
+    labelArray = pro_processing(id) 
+    # np.set_printoptions(threshold='nan')
+    # print labelArray
